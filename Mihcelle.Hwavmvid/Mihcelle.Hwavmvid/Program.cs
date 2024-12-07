1d9c3c182125bcb1285a9eee1359e7b0905c9a45
boredgaaal <mihcelle@hotmail.com>
2024-12-07 13:46:08 +0100

    refactored and stored data from webassembly app

--- Mihcelle.Hwavmvid/Mihcelle.Hwavmvid/Program.cs
+++ Mihcelle.Hwavmvid/Mihcelle.Hwavmvid/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using System.Text.Json;
 using Mihcelle.Hwavmvid.Components;
+using Mihcelle.Hwavmvid.Shared.Constants;
+using Mihcelle.Hwavmvid.Shared.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +11,56 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+
+
+
+// mihcelle.hwavmvid
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var installed = !string.IsNullOrEmpty(connectionString);
+
+if (installed == false)
+{
+    var configpath = string.Concat(builder.Environment.ContentRootPath, "\\wwwroot\\", "tobaccoindustries.json");
+    var jsonconfig = System.IO.File.ReadAllText(configpath);
+    var deserializedconfig = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonconfig);
+    if (deserializedconfig != null)
+    {
+        deserializedconfig["installation"] = new { createdon = string.Empty };
+        var updatedconfigfile = JsonSerializer.Serialize(deserializedconfig, new JsonSerializerOptions { WriteIndented = true });
+        System.IO.File.WriteAllText(configpath, updatedconfigfile);
+    }
+}
+
+try
+{
+    builder.Services.AddDbContext<Mihcelle.Hwavmvid.Server.Data.Applicationdbcontext>(options => options.UseSqlServer(connectionString));
+    builder.Services.AddIdentity<Applicationuser, IdentityRole>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 2;
+    })
+        .AddEntityFrameworkStores<Mihcelle.Hwavmvid.Server.Data.Applicationdbcontext>();
+
+    builder.Services.ConfigureApplicationCookie(options =>
+    {
+        options.Cookie.Name = Authentication.Authcookiename;
+        options.Cookie.HttpOnly = false;
+        options.SlidingExpiration = true;
+        options.ExpireTimeSpan = TimeSpan.FromDays(1);
+    });
+}
+catch (Exception exception) { Console.WriteLine(exception.Message); }
+
+
+
+
+
+
 
 var app = builder.Build();
 
